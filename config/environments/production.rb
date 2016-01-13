510c18867f5bacd96f8e9bb68c23af52175f4ef2
Devin Mois <dwmois7@gmail.com>
2016-01-13 19:23:37 +0000

    Add devise gem and user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Jamster::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'jamster-devinm.herokuapp.com'}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
