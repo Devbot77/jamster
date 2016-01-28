fb2fa0625bcda328261bfbc3c8db177cc826dd99
Devin Mois <dwmois7@gmail.com>
2016-01-28 19:20:57 +0000

    Add enrollment authentication to lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,21 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lesson, only: [:show]
+
 	def show
 	end
 
 	private
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+	def require_authorized_for_current_lesson
+		this_course = current_lesson.section.course
+		if current_user.enrolled_in?(this_course) == false
+			redirect_to course_path(this_course), alert: 'You must enroll to view the lesson'
+		end
 	end
 end
